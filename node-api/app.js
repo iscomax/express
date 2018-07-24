50cfc3c3d5d2047826db5b079901dad4ce3f8cf3
iscomax <santamariajc61@gmail.com>
2018-07-24 11:39:07 -0500

    modificacion archivo app.js

--- node-api/app.js
+++ node-api/app.js
@@ -10,6 +10,13 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
 
 routes(app);
 
@@ -20,3 +27,7 @@ var server = app.listen(3000, function () {
 mongoose.connect('mongodb://127.0.0.1:27017/todo-list')
 .then(()=>{console.log('Succesfully Connected to the Mongodb Database at ')})
 .catch(()=>{console.log('data base error')})
+
+
+
+
