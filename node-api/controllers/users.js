65d4cb546cf2cec694cd9ee6ecfb99806b5b6a6a
iscomax <santamariajc61@gmail.com>
2018-07-24 10:44:16 -0500

    actulaizacion de la api

--- node-api/controllers/users.js
+++ node-api/controllers/users.js
@@ -1,4 +1,4 @@
-var User = require('../models/user.js');
+var User = require('../models/user');
 
 exports.getUsers = (req, res, next) => {
   User.find((err, users) => {
@@ -7,6 +7,60 @@ exports.getUsers = (req, res, next) => {
   }
   else {
     res.status(200).send(users);
+  }
+  });
+}
+
+exports.createUser = (req, res, next) => {
+  let newUser = new User(req.body);
+  newUser.save((err, user) => {
+  if (err) {
+    next(new Error(err));
+  }
+  else {
+    res.status(201).send(user);
+  }
+  });
+}
+
+exports.getUser = (req, res, next) => {
+  let id = req.params.id
+  User.findOne({_id: id}, (err, user) => {
+  if (err) {
+    next(new Error(err));
+  }
+  else {
+    res.status(200).send(user);
+  }
+  });
+}
+
+exports.updateUser = (req, res, next) => {
+  let id = req.params.id;
+  User.findOne({_id: id}, (err, user) => {
+  if (err) {
+    next(new Error(err));
+  }
+  else {
+    let updatedUser = new User(req.body);
+    user.username = updatedUser.username;
+    user.first_name = updatedUser.first_name;
+    user.last_name = updatedUser.last_name;
+    user.save();
+    res.status(200).send(user);
+  }
+  });
+}
+
+exports.deleteUser = (req, res, next) => {
+  let id = req.params.id;
+  User.findOne({_id: id}, (err, user) => {
+  if (err) {
+    next(new Error(err));
+  }
+  else {
+    user.remove();
+    res.status(204).send("Deleted");
   }
   });
 }
--- node-api/routes/routes.js
+++ node-api/routes/routes.js
@@ -1,29 +1,52 @@
 var faker = require("faker");
-
-var UserController = require("../controllers/users");
-var TaskController = require("../controllers/task");
+var userController = require("../controllers/users");
+var taskController = require('../controllers/tasks');
 
 var appRouter = function (app) {
 
   app.get("/", function(req, res) {
-    res.status(200).send("Welcome to our restful API");
+    res.status(200).send("REST API");
   });
 
-  app.get("/users", function (req, res, next) {
-    UserController.getUsers(req, res, next);
-    });
-    
-    
-    
-  app.get("/tasks", function (req, res, next) {
-    TaskController.getTasks(req, res, next);
-    });
-  };
-  
+  app.get("/users", (req, res, next) => {
+    userController.getUsers(req, res, next);
+  });
+
+  app.post("/users", (req, res, next) => {
+    userController.createUser(req, res, next);
+  });
+
+  app.get("/users/:id", (req, res, next) => {
+    userController.getUser(req, res, next);
+  });
+
+  app.put("/users/:id", (req, res, next) => {
+    userController.updateUser(req, res, next);
+  });
+
+  app.delete("/users/:id", (req, res, next) => {
+    userController.deleteUser(req, res, next);
+  });
+
+  app.get("/tasks", (req, res, next) => {
+    taskController.getTasks(req, res, next);
+  });
+
+  app.post("/tasks", (req, res, next) => {
+    taskController.createTask(req, res, next);
+  });
+
+  app.get("/tasks/:id", (req, res, next) => {
+    taskController.getTask(req, res, next);
+  });
+
+  app.put("/tasks/:id", (req, res, next) => {
+    taskController.updateTask(req, res, next);
+  });
+
+  app.delete("/tasks/:id", (req, res, next) => {
+    taskController.deleteTask(req, res, next);
+  });
+}
 
 module.exports = appRouter;
-
-
-
-
-
